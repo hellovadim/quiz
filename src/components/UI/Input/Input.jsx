1f2aa612f31ad206f7cb631dc3cb5cd4e7acb690
Vadim <hellovados@gmail.com>
2021-08-13 19:09:41 +0300

    добавил компонент инпут и форму авторизации

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,27 @@
+import styles from "./Input.module.css";
+
+const Input = (props) => {
+  const inputType = props.type || "text";
+  const cls = [styles.Input];
+  const htmlFor = `${inputType} - ${Math.random()}`;
+
+function isInvalid({valid, touched, shouldValidate}) {
+    return !valid && shouldValidate && touched
+}
+
+ if(isInvalid.props) {
+     cls.push(styles.invalid)
+ }
+  return (
+    <div className={cls.join(" ")}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input value={props.value} type={inputType} onChange={props.onChange} />
+      {
+          isInvalid(props) ?  <span>{props.errorMessage} || 'Введите верное значение'</span> : null
+      }
+     
+    </div>
+  );
+};
+
+export default Input;
--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import styles from "./Auth.module.css";
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export class Auth extends Component {
-    
-
-    render() {
-        return (
-            <div>
-               <h1>Auth</h1> 
-            </div>
-        )
-    }
+  loginHandler = () => {};
+  registrHandler = () => {};
+  submitHandler = () => {};
+  render() {
+    return (
+      <div className={styles.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form className={styles.AuthForm} onSubmit={this.submitHandler}>
+            <Input label="Email" />
+            <Input 
+            label="Пароль"
+            errorMessage={'Test'}
+            />
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registrHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Auth
+export default Auth;
