55bdfbc8c7ee07f8b6a48ec4c6ee11f5b8e37497
Vadim <hellovados@gmail.com>
2021-08-26 15:13:35 +0300

    функция добавления вопроса

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -62,8 +62,37 @@ class QuizCreator extends Component {
   };
   addQuestionHandler = (event) => {
     event.preventDefault();
+
+    const quiz = this.state.quiz.concat();
+    const index = quiz.length + 1;
+    const { question, option1, option2, option3, option4 } =
+      this.state.formControls;
+    const questionItem = {
+      question: question.value,
+      id: index,
+      rightAnswerId: this.state.rightAnswerId,
+      answers: [
+        { text: option1.value, id: option1.id },
+        { text: option2.value, id: option2.id },
+        { text: option3.value, id: option3.id },
+        { text: option4.value, id: option4.id },
+      ],
+    };
+    quiz.push(questionItem);
+
+    this.setState({
+      quiz,
+      rightAnswerId: 1,
+      isFormValid: false,
+      formControls: createFormControls(),
+    });
   };
-  createQuizHandler = () => {};
+  createQuizHandler = (event) => {
+    event.preventDefault()
+
+    console.log(this.state.quiz)
+
+  };
   renderInputs() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
       const control = this.state.formControls[controlName];
